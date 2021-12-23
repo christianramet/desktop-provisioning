aca0aa54cbeb2a0b36b643d73a151ec867034f04
Christian Ramet <git@christianramet.com>
2021-12-23 14:00:26 +0100

    do not write the env for brew yet

--- mac-bootstrap.zsh
+++ mac-bootstrap.zsh
@@ -29,7 +29,6 @@ fi
 
 if [ "$BREW" != installed ]; then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/chris/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
